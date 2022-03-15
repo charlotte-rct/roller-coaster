d8796d64b3806b6813e2fcd83ff1e2d8d8763c9e
clementH <henichardclement@gmail.com>
2022-03-15 23:10:08 +0100

    Add second test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { getValuesFromFile } from './parser';
 import { calculateTotalEarnings } from './index';
 
-test('Simple test case return 7', () => {
+test('1 - Simple test case should return 7', () => {
   // Given
   const data = getValuesFromFile('./samples/1_simple_case.txt');
 
@@ -10,4 +10,15 @@ test('Simple test case return 7', () => {
 
   // Then
   expect(result).toBe(7);
+});
+
+test('2 - 1000 groups of few people should return 3935', () => {
+  // Given
+  const data = getValuesFromFile('./samples/2_1000_groups_of_few_people.txt');
+
+  // When
+  const result = calculateTotalEarnings(data);
+
+  // Then
+  expect(result).toBe(3935);
 });
